7792f9972a8e6728b2e7e903de3b30fcb649f67e
Csaba Vadász <csabavadasz79@gmail.com>
2025-03-17 19:32:51 +0100

    add main with a short explanation

---
+++ src/main/java/com/csaba79coder/solid/SolidMain.java
@@ -0,0 +1,14 @@
+package com.csaba79coder.solid;
+
+public class SolidMain {
+
+    public static void main(String[] args) {
+        /*
+        - Single Responsible Principle (SRP) - egy osztály csak egy felelősséget kap
+        - Open/Close Principle (OCP) - nyitott a bővítésre, de zárt a módosításra
+        - Liskov Substitution Principle (LSP) - alosztály helyettesitheti az ősosztályt hiba nélkül
+        - Interface Segregation Principle (ISP) - kis, specifikus interface-ek (nem kényszerítünk rájuk semmi extra funkciót, ami nem kell)
+        - Dependency Inversion Principly (DIP) - megvalósítása a dependency injection  (mező, konstruktor, setter) - az abstrakcióra építsünk, ne a konkrét implementációra
+        */
+    }
+}
